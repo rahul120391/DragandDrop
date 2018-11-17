b39267ac8ccf6a20ffcf4c4bfcfb723756c72235
rahul <rahul.kumar.gautam1291@gmail.com>
2018-11-17 16:08:36 +0530

    changes in drag and drop

--- app/src/main/java/com/example/jkuma86/draganddrop/MainActivity.kt
+++ app/src/main/java/com/example/jkuma86/draganddrop/MainActivity.kt
@@ -3,9 +3,11 @@ package com.example.jkuma86.draganddrop
 import android.content.ClipData
 import android.content.ClipDescription
 import android.graphics.Color
+import android.graphics.drawable.Drawable
 import android.os.Build
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.content.ContextCompat
 import android.view.DragEvent
 import android.view.DragEvent.*
 import android.view.View
@@ -23,10 +25,10 @@ class MainActivity : AppCompatActivity(), View.OnLongClickListener, View.OnDragL
     private val mFourImage by lazy { findViewById<RoundedImageView>(R.id.imgFour) }
     private val mFiveImage by lazy { findViewById<RoundedImageView>(R.id.imgFive) }
     private val mSixImage by lazy { findViewById<RoundedImageView>(R.id.imgSix) }
-    private lateinit var mSourceImageView:RoundedImageView
-    private lateinit var mDestImageView:RoundedImageView
-    private var mSourceImageResource:Int = 0
-    private var mDestImageResource:Int=0
+    private lateinit var mSourceImageView: RoundedImageView
+    private lateinit var mDestImageView: RoundedImageView
+    private var mSourceImageResource: Drawable?=null
+    private var mDestImageResource: Drawable? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -56,104 +58,13 @@ class MainActivity : AppCompatActivity(), View.OnLongClickListener, View.OnDragL
     override fun onLongClick(view: View): Boolean {
 
         when (view.id) {
-            R.id.imgFirst -> {
-
-                println("first image long click")
-                val item = ClipData.Item(view.tag as CharSequence)
-                val dragData = ClipData(
-                    view.tag as? CharSequence,
-                    arrayOf(ClipDescription.MIMETYPE_TEXT_PLAIN),
-                    item
-                )
-                // Instantiates the drag shadow builder.
-                val myShadow = MyDragShadowBuilder(view)
-
-                // Starts the drag
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
-                    mSourceImageView=mFirstImage
-                    mSourceImageResource=R.drawable.nature1
-                    view.startDragAndDrop(dragData, myShadow, null, 0)
-                }
-
-            }
-            R.id.imgSecond -> {
-                println("second image long click")
-                val item = ClipData.Item(view.tag as CharSequence)
-                val dragData = ClipData(
-                    view.tag as? CharSequence,
-                    arrayOf(ClipDescription.MIMETYPE_TEXT_PLAIN),
-                    item
-                )
-                // Instantiates the drag shadow builder.
-                val myShadow = MyDragShadowBuilder(view)
-
-                // Starts the drag
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
-                    mSourceImageView=mSecondImage
-                    mSourceImageResource=R.drawable.nature2
-                    view.startDragAndDrop(dragData, myShadow, null, 0)
-                }
-
-            }
-            R.id.imgThird -> {
-                println("third image long click")
-                val item = ClipData.Item(view.tag as CharSequence)
-                val dragData = ClipData(
-                    view.tag as? CharSequence,
-                    arrayOf(ClipDescription.MIMETYPE_TEXT_PLAIN),
-                    item
-                )
-                // Instantiates the drag shadow builder.
-                val myShadow = MyDragShadowBuilder(view)
-
-                // Starts the drag
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
-                    mSourceImageView=mThirdImage
-                    mSourceImageResource=R.drawable.nature3
-                    view.startDragAndDrop(dragData, myShadow, null, 0)
-                }
-
-            }
-            R.id.imgFour -> {
-                println("fourth image long click")
-                val item = ClipData.Item(view.tag as CharSequence)
-                val dragData = ClipData(
-                    view.tag as? CharSequence,
-                    arrayOf(ClipDescription.MIMETYPE_TEXT_PLAIN),
-                    item
-                )
-                // Instantiates the drag shadow builder.
-                val myShadow = MyDragShadowBuilder(view)
-
-                // Starts the drag
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
-                    mSourceImageView=mFourImage
-                    mSourceImageResource=R.drawable.nature4
-                    view.startDragAndDrop(dragData, myShadow, null, 0)
-                }
-
-            }
-            R.id.imgFive -> {
-                println("fifth image long click")
-                val item = ClipData.Item(view.tag as CharSequence)
-                val dragData = ClipData(
-                    view.tag as? CharSequence,
-                    arrayOf(ClipDescription.MIMETYPE_TEXT_PLAIN),
-                    item
-                )
-                // Instantiates the drag shadow builder.
-                val myShadow = MyDragShadowBuilder(view)
-
-                // Starts the drag
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
-                    mSourceImageView=mFiveImage
-                    mSourceImageResource=R.drawable.nature5
-                    view.startDragAndDrop(dragData, myShadow, null, 0)
-                }
-
-            }
+            R.id.imgFirst,
+            R.id.imgSecond,
+            R.id.imgThird,
+            R.id.imgFour,
+            R.id.imgFive,
             R.id.imgSix -> {
-                println("sixth image long click")
+                println("tag is ${view.tag}")
                 val item = ClipData.Item(view.tag as CharSequence)
                 val dragData = ClipData(
                     view.tag as? CharSequence,
@@ -162,17 +73,17 @@ class MainActivity : AppCompatActivity(), View.OnLongClickListener, View.OnDragL
                 )
                 // Instantiates the drag shadow builder.
                 val myShadow = MyDragShadowBuilder(view)
-
                 // Starts the drag
                 if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
-                    mSourceImageView=mSixImage
-                    mSourceImageResource=R.drawable.nature6
                     view.startDragAndDrop(dragData, myShadow, null, 0)
                 }
-
+                mSourceImageView = view as RoundedImageView
+                println("drawable ${mFirstImage.drawable.constantState}")
+                mSourceImageResource = mSourceImageView.drawable
             }
 
         }
+
 
         return true
     }
@@ -187,44 +98,20 @@ class MainActivity : AppCompatActivity(), View.OnLongClickListener, View.OnDragL
 
             }
             ACTION_DROP -> {
-                mSourceImageView.invalidate()
-                mDestImageView.invalidate()
-                mSourceImageView.setImageResource(mDestImageResource)
-                mDestImageView.setImageResource(mSourceImageResource)
+                if (mSourceImageView != mDestImageView) {
+                    mSourceImageView.setImageDrawable(mDestImageResource)
+                    mDestImageView.setImageDrawable(mSourceImageResource)
+                  //  mSourceImageView.setImageResource(mDestImageResource)
+                   // mDestImageView.setImageResource(mSourceImageResource)
+                }
                 return true
             }
             ACTION_DRAG_EXITED -> {
 
             }
             ACTION_DRAG_ENTERED -> {
-                println("entered")
-                when(view?.id){
-                    R.id.imgFirst->{
-                        mDestImageView=mFirstImage
-                        mDestImageResource=R.drawable.nature1
-                    }
-                    R.id.imgSecond->{
-                        mDestImageView=mSecondImage
-                        mDestImageResource=R.drawable.nature2
-                    }
-                    R.id.imgThird->{
-                        mDestImageView=mThirdImage
-                        mDestImageResource=R.drawable.nature3
-                    }
-                    R.id.imgFour->{
-                        mDestImageView=mFourImage
-                        mDestImageResource=R.drawable.nature4
-                    }
-                    R.id.imgFive->{
-                        mDestImageView=mFiveImage
-                        mDestImageResource=R.drawable.nature5
-                    }
-                    R.id.imgSix->{
-                        mDestImageView=mSixImage
-                        mDestImageResource=R.drawable.nature6
-                    }
-                }
-
+                mDestImageView = view as RoundedImageView
+                mDestImageResource = mDestImageView.drawable
                 return true
             }
         }
